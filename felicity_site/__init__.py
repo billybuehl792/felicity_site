308a5e4ada38d302e0b28d1beb7ad28df14fa73d
Billy Buehl <billybuehl792@gmail.com>
2020-12-25 17:21:34 -0500

    init file edited

--- felicity_site/__init__.py
+++ felicity_site/__init__.py
@@ -6,15 +6,11 @@ from flask import Flask, url_for
 # from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'velocity_facility_faculty2020'
+
 config_file = 'felicity_site/static/config/media_config.json'
 print(config_file)
 with open(config_file, 'r') as f:
     media_config = json.load(f)
-
-# app.config['SECRET_KEY'] = 'fbcf9b74745faa8d224628eabba4222b'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
 
 from felicity_site import routes
